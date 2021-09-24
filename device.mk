e1039c2b85a125e90cba7577733c95ef2cd6e8b7
mastersenpai05 <pampangavergil0405@gmail.com>
2021-09-24 08:28:41 +0800

    camellia: update libs and blobs needed in recovery

--- device.mk
+++ device.mk
@@ -24,10 +24,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1-0-impl \
-    android.hardware.boot@1.0 \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.impl
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
